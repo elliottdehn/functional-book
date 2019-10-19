dee96734c3bcceb364dd920a7dc9c772aabc589e
elliottdehnbostel <dehnbostele@gmail.com>
2019-10-19 13:35:55 -0500

    getting started complete

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -182,13 +182,17 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => (b: B) => f(a, b)
+
+  //Answer compare: I did not need the type annotations.
 
   // NB: The `Function2` trait has a `curried` method already
 
   // Exercise 4: Implement `uncurry`
   def uncurry[A,B,C](f: A => B => C): (A, B) => C =
-    ???
+    (a,b) => f(a)(b)
+
+  //Answer compare: dead on!
 
   /*
   NB: There is a method on the `Function` object in the standard library,
@@ -203,5 +207,7 @@ object PolymorphicFunctions {
   // Exercise 5: Implement `compose`
 
   def compose[A,B,C](f: B => C, g: A => B): A => C =
-    ???
+    a => f(g(a))
+
+  //answer compare: dead on!
 }
