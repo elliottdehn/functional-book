94d63c01f81318d705e177fc2fae76e13043350a
elliottdehnbostel <dehnbostele@gmail.com>
2019-10-20 15:07:32 -0500

    did a bunch of exercises up to and including map

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,4 +1,4 @@
-package fpinscala.datastructures
+//package fpinscala.datastructures
 
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
@@ -96,7 +96,71 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(l, 0)((_, y) => 1 + y)
   }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  //Answer comparison: I incorrectly had a case for Cons(h, Nil)
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h1, t) => foldLeft(t, f(z, h1))(f)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  //Answer compares to all 3: I had a case for Nil, but didn't need it
+  def sum3(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case _ => foldLeft(ints, 0)(_ + _)
+  }
+
+  //Nil should have returned 1.0, anyway
+  def product3(ds: List[Double]): Double = ds match {
+    case Nil => 0
+    case _ => foldLeft(ds, 1.0)(_ * _)
+  }
+
+  def length2[A](l: List[A]): Int = l match {
+    case Nil => 0
+    case _ => foldLeft(l, 0)((x, _) => x + 1)
+  }
+
+  //Answer compare: close. I used a Nil case when I could have used a one-liner with List[A]() for z
+  def reverse[A](l: List[A]): List[A] = {
+    case Nil => Nil
+    case Cons(h, _) => foldLeft(l, Cons(h, Nil))((x,y) => Cons(y, x))
+  }
+
+  def append_fl[A](l: List[A], app: List[A]): List[A] = {
+    foldLeft(reverse(l), app)((acc: List[A], x: A) => Cons(x, acc))
+  }
+
+  //Compared to answer: correct. I should have simply used f = Cons(_,_)
+  def append_fr[A](l: List[A], app: List[A]): List[A] = {
+    foldRight(l, app)((x, acc) => Cons(x, acc))
+  }
+
+  //not tail recursive, but correct?
+  def flatten[A](l: List[List[A]]): List[A] = l match {
+    case Nil => Nil
+    case Cons(fl, t) => append(fl, flatten(t))
+  }
+
+  //Answer comparison: I did not use fold right, but I think this is correct
+  //It is functionally in the same vein: concatenate the first list with the second
+  //and do this until you run out of lists. Return the flattened flat list.
+  def flatten_tr[A](l: List[List[A]]): List[A] = l match {
+    case Nil => Nil
+    case Cons(fl, Nil) => fl
+    case Cons(fl, Cons(sl, t)) => flatten_tr(Cons(append(fl, sl), t))
+  }
+
+  //Answer comparison: dead on!
+  def inc_one(l: List[Int]): List[Int] = {
+    foldRight(l, Nil:List[Int])((ne, acc) => Cons(ne + 1, acc))
+  }
+
+  //Answer comparison: dead on!
+  def d_ts(l: List[Double]): List[String] = {
+    foldRight(l, Nil:List[String])((ne, acc) => Cons(ne.toString, acc))
+  }
+
+  //Answer comparison: I did the "natural" solution but the typical sol. is a buffer
+  def map[A,B](l: List[A])(f: A => B): List[B] = {
+    foldRight(l, Nil:List[B])((ne, acc) => Cons(f(ne), acc))
+  }
 }
