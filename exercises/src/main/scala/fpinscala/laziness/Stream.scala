801f61fd2b7254b24aa5535cba01ddd8d3dd91ee
elliottdehnbostel <dehnbostele@gmail.com>
2019-10-23 22:56:42 -0500

    finished up for the night

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -85,8 +85,51 @@ trait Stream[+A] {
 
   //Shame: I gave up on this problem and looked at the answer - turns out I was missing a key concept.
 
-  
-  def startsWith[B](s: Stream[B]): Boolean = ???
+  def takeViaUnfold(n: Int): Stream[A] =
+    unfold((this,n)) {
+      case (Cons(h,t), 1) => Some((h(), (empty, 0)))
+      case (Cons(h,t), n) if n > 1 => Some((h(), (t(), n-1)))
+      case _ => None
+    }
+
+  def takeWhileViaUnfold(f: A => Boolean): Stream[A] =
+    unfold(this) {
+      case Cons(h,t) if f(h()) => Some((h(), t()))
+      case _ => None
+    }
+
+  def zipWith[B,C](s2: Stream[B])(f: (A,B) => C): Stream[C] =
+    unfold((this, s2)) {
+      case (Cons(h1,t1), Cons(h2,t2)) =>
+        Some((f(h1(), h2()), (t1(), t2())))
+      case _ => None
+    }
+
+  // special case of `zipWith`
+  def zip[B](s2: Stream[B]): Stream[(A,B)] =
+    zipWith(s2)((_,_))
+
+
+  def zipAll[B](s2: Stream[B]): Stream[(Option[A],Option[B])] =
+    zipWithAll(s2)((_,_))
+
+  def zipWithAll[B, C](s2: Stream[B])(f: (Option[A], Option[B]) => C): Stream[C] =
+    Stream.unfold((this, s2)) {
+      case (Empty, Empty) => None
+      case (Cons(h, t), Empty) => Some(f(Some(h()), Option.empty[B]) -> (t(), empty[B]))
+      case (Empty, Cons(h, t)) => Some(f(Option.empty[A], Some(h())) -> (empty[A] -> t()))
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(f(Some(h1()), Some(h2())) -> (t1() -> t2()))
+    }
+
+  def startsWith[B](s: Stream[B]): Boolean = {
+    this.zipWithAll(s)((l, r) => (l == r) || r.isEmpty).forAll(b => b)
+  }
+
+  def tails: Stream[Stream[A]] = {
+    Stream.unfold(this)(s => Some(s, s.drop(1)))
+  }
+
+  def scanRight[B](z: B)(f: (A, B) => B): Stream[B] = ???
 }
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
