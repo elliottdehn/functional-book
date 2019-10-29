563840a9ad188aadfb74399d790adadebe1f8ed9
elliottdehnbostel <dehnbostele@gmail.com>
2019-10-28 19:56:32 -0500

    running into a strange type error with this exercise...

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,12 +1,8 @@
 package fpinscala.testing
 
-import fpinscala.laziness.Stream
-import fpinscala.state._
-import fpinscala.parallelism._
-import fpinscala.parallelism.Par.Par
-import Gen._
-import Prop._
-import java.util.concurrent.{Executors,ExecutorService}
+import fpinscala.state
+import fpinscala.state.RNG
+import fpinscala.state.RNG.State
 
 /*
 The library developed in this chapter goes through several iterations. This file is just the
@@ -14,14 +10,29 @@ shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
+  def check: Boolean
+  /*
+  def &&(p: Prop): Boolean = {
+   check && p.check
+  }
+  */
+
+  /* We can refer to the enclosing `Prop` instance with `Prop.this` */
+  def &&(p: Prop): Prop = new Prop {
+    def check: Boolean = Prop.this.check && p.check
+  }
 }
 
 object Prop {
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
+case class Gen[+A](sample: State[RNG, A])
+
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
+  }
 }
 
 trait Gen[A] {
