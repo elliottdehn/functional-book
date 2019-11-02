875217a00029b816730c78caaecacb01aeaf131b
elliottdehnbostel <dehnbostele@gmail.com>
2019-11-01 19:25:26 -0500

    adding some of the headers from the chapter before starting the json parser

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -1,15 +1,38 @@
 package fpinscala.parsing
 
+import fpinscala.testing._
+
 import language.higherKinds
 
 trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
 
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
+  def char(c: Char): Parser[Char]
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  //implicits
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]): ParserOps[A] = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
+    ParserOps[String] = ParserOps(f(a))
+
+
+
   case class ParserOps[A](p: Parser[A]) {
+    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def or[B>:A](p2: => Parser[B]): Parser[B] = self.or(p, p2)
 
-
+    def many[B](p: Parser[A])(f: A => B): Parser[B] = ???
+    def listOfN(n: Int, p: Parser[A]): Parser[List[A]] = ???
+    def map[B](a: Parser[A])(f: A => B): Parser[B] = ???
   }
 
   object Laws {
+    def equal[A](p1: Parser[A], p2: Parser[A])(in: Gen[String]): Prop =
+      Prop.forAll(in)(s => run(p1)(s) == run(p2)(s))
+
+    def mapLaw[A](p: Parser[A])(in: Gen[String]): Prop =
+      equal(p, map(p)(a => a))(in)
   }
 }
 
